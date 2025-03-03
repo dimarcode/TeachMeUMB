7baaee6579f4dd1eab13b586e135da6ba0a5536c
Cody <codydimarcolr@gmail.com>
2025-03-03 16:26:05 +0000

    First commit

---
+++ php/www/index.php
@@ -0,0 +1,17 @@
+ <!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+<?php
+
+mysqli_connect("db", "root", "hunter2") or die(mysqli_error());
+echo "Connected to MySQL test
+";
+
+?>
+
+</body>
+</html> 
